00c77b2ae7a0fd270fd0fdf88bb225ebef2a1be7
appleskiller@163.com <appleskiller@163.com>
2020-02-13 21:59:20 +0800

    build: auto commit before release

--- src/datatype/index.ts
+++ src/datatype/index.ts
@@ -1,12 +1,14 @@
 
 
-import * as hierarchyExports from './hierarchy';
 // -------------------------------------------------------------------
 // datatype
 // ===================================================================
+import * as gridExports from './gridmatix';
+import * as hierarchyExports from './hierarchy';
 import * as hierarchycollectionExports from './hierarchycollection';
 import * as hierarchygridExports from './hierarchygrid';
 
+export const gridmatix = gridExports;
 export const hierarchy = hierarchyExports;
 export const hierarchycollection = hierarchycollectionExports;
 export const hierarchygrid = hierarchygridExports;
