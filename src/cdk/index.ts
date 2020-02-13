b8f3509c45bab0906e896dfa309cb4a4c4df5989
appleskiller@163.com <appleskiller@163.com>
2020-02-13 20:15:55 +0800

    build: auto commit before release

--- src/cdk/index.ts
+++ src/cdk/index.ts
@@ -1,6 +1,6 @@
-
 export { Animation } from "./animation";
 export { dragManager } from './dragdrop/manager';
 export { popupManager } from './popup/manager';
 export { scrollManager } from './scroll/manager';
 
+export { DropPosition } from './dragdrop/interfaces';
--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,8 @@ export {
 export { Animation,
     dragManager,
     popupManager,
-    scrollManager
+    scrollManager,
+    DropPosition
 } from './cdk';
 // -------------------------------------------------------------------
 // components
