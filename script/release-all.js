995a3b3c062e51d461d86c7dff40a6d44d662d40
unknown <appleskiller@163.com>
2020-12-02 00:36:13 +0800

    build: auto commit before release

--- script/release-all.js
+++ script/release-all.js
@@ -1,26 +1,36 @@
 
 const util = require('./util');
 
-util.cd('../neurons-xml');
-util.exec('npm run release');
+var args = util.parseArgs();
+var registry = args.registry === 'taobao' ? 'https://registry.npm.taobao.org' : 'https://registry.npmjs.org';
+var registryArgs = args.registry === 'taobao' ? '-- --registry taobao' : '';
 
-util.cd('../neurons-injector');
-util.exec('npm run release');
+// util.cd('../neurons-xml');
+// util.exec(`npm run release ${registryArgs}`);
 
-util.cd('../neurons-utils');
-util.exec('npm run release');
+// util.cd('../neurons-injector');
+// util.exec(`npm run release ${registryArgs}`);
 
-util.cd('../neurons-animation');
-util.exec('npm run release');
+// util.cd('../neurons-utils');
+// util.exec(`npm run release ${registryArgs}`);
 
-util.cd('../neurons-sort');
-util.exec('npm run release');
 
-util.cd('../neurons-emitter');
-util.exec('npm run release');
 
-util.cd('../neurons-dom');
-util.exec('npm run release');
+
+// util.cd('../neurons-animation');
+// util.exec(`npm run release ${registryArgs}`);
+
+// util.cd('../neurons-sort');
+// util.exec(`npm run release ${registryArgs}`);
+
+// util.cd('../neurons-emitter');
+// util.exec(`npm run release ${registryArgs}`);
+
+// util.cd('../neurons-dom');
+// util.exec(`npm run release ${registryArgs}`);
+
+
+
 
 util.cd('../neurons');
-util.exec('npm run release');
+util.exec(`npm run release ${registryArgs}`);
